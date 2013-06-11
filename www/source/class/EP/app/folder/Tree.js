7a93f0193a35358e7d6f9bc89c7ebea0fb11e5d5
franck34 <eviltik@gmail.com>
2013-06-11 11:16:59 +0200

    move sound routine into taskBar

--- www/source/class/EP/app/folder/Tree.js
+++ www/source/class/EP/app/folder/Tree.js
@@ -10,39 +10,12 @@ qx.Class.define("EP.app.folder.Tree", {
         this.__workspaceData = workspaceData;
         this.base(arguments);
         this.addListener('jobMessage',this.onJobMessage,this);
-
-        if (qx.core.Environment.get("html.audio.mp3")) {
-            var u = qx.util.ResourceManager.getInstance().toUri("EP/sound/6370.mp3");
-            this.__sounds = [];
-            this.__soundPointer = 0;
-            for (var i = 0; i<10 ; i++) {
-                // preload
-                qx.lang.Function.delay(function() {
-                    this.__sounds.push(new qx.bom.media.Audio(u));
-                },i*100,this);
-            }
-        }
-
     },
 
     members:{
         __workspaceData:null,
-        __beepUri: null,
-
-        playSound:function() {
-            var beep = this.__sounds[this.__soundPointer];
-            var v = qx.core.Init.getApplication().__soundVolume;
-            beep.setVolume(v);
-            beep.play();
-            this.__soundPointer++;
-            if (this.__soundPointer == 10) {
-                this.__soundPointer = 0;
-            }
-        },
 
         onJobMessage:function(ev) {
-            this.playSound();
-
             var node = JSON.parse(JSON.stringify(ev.getData().message.node));
             var parentId = node.parent;
             node = this.extendDataItem(node);
