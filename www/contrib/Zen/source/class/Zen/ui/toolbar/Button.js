ce78b5cd6d520dba281330fc30f7734a4246497e
eviltik <eviltik@gmail.com>
2013-07-26 10:17:40 +0200

    remove Zen contrib

--- www/contrib/Zen/source/class/Zen/ui/toolbar/Button.js
+++
@@ -1,7 +0,0 @@
-qx.Class.define("Zen.ui.toolbar.Button", {
-    extend : qx.ui.toolbar.Button,
-    construct:function(label,icon,command) {
-    	this.base(arguments,label,icon,command);
-    	this.setCursor('pointer');
-    }
-});
